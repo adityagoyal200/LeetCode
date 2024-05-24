f45a70c70c73da659893bf34f266b51fee9f0982
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-05-24 09:15:31 +0530

    Time: 1 ms (99.81%), Space: 53 MB (43.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,20 +3,29 @@ class Solution {
         if(nums == null || nums.length == 0){
             return -1;
         }
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num: nums){
-            if(!map.containsKey(num)){
-                map.put(num,0);
+        int ele = nums[0];
+        int count = 1;
+        int i = 1;
+        while(i < nums.length){
+            if(count == 0){
+                ele = nums[i];
+                count = 1;
             }
-            map.put(num,map.get(num)+1);
+            else if(nums[i] == ele){
+                count++;
+            } else {
+                count--;
+            }
+            i++;
         }
-        int majority = (nums.length)/2;
-        for(int num: map.keySet()){
-            if(map.get(num) > majority){
-                return num;
+        /**check if ele  is majority*/
+        int majority = nums.length /2;
+        int cnt = 0;
+        for(int j = 0; j< nums.length; j++){
+            if(nums[j] == ele){
+                cnt++;
             }
         }
-
-        return -1;
+        return (cnt > majority) ? ele: -1;
     }
 }
