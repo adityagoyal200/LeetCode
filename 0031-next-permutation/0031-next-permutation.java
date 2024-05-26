8c277d52c6309fd572b6f1734ddefc907b6cb1f3
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-05-26 23:41:04 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (65.18%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,28 +1,31 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        // find the break point where i+1 > i
         int len = nums.length;
-        int point = -1;
-        for(int i = len-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                point = i;
-                break;
-            }
-        }
-        if(point == -1){
-            reverse(nums,0,len-1);
+        if(nums == null || len == 0){
             return;
         }
-        // find slightly greater and swap
-        for(int i = len-1;i>=0;i--){
-            if(nums[i]>nums[point]){
-                int temp = nums[point];
-                nums[point] = nums[i];
-                nums[i] = temp;
+        int index = -1;
+        for(int i = len-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
                 break;
             }
         }
-        reverse(nums,point+1,len-1);      
+        if(index == -1){
+            reverse(nums,0 ,len-1);
+            return;
+        }
+        /* find the greater closest element*/
+        for(int i = len-1; i > index; i--){
+            if(nums[i]> nums[index]){
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                break;
+            }
+        }
+        /* now just reverse the index+1 numsay to get the next permutation */
+        reverse(nums,index+1,len-1);
     }
     public void reverse(int[] nums , int start , int end){
         while(start<end){
