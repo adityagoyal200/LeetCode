726e774142226adfb699408eddd07f8676a94421
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-06-05 21:29:23 +0530

    Time: 15 ms (82.8%), Space: 43.5 MB (94.01%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -5,28 +5,47 @@ class Solution {
         }
         int n = nums.length;
         List<List<Integer>> result = new ArrayList<>();
-        Set<List<Integer>> quad = new HashSet<>();
+        Arrays.sort(nums);
 
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                HashSet<Long> set = new HashSet<>();
-                for(int k = j+1; k<n; k++){
-                    long sum = nums[i]+nums[j];
-                    sum+= nums[k];
-                    long rem = target - sum;
-                    if(set.contains(rem)){
-                        List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k],(int)rem);
-                        Collections.sort(temp);
-                        quad.add(temp);
+        for(int i = 0 ; i < n; i++){
+            if(i!= 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            for(int j = i+1; j < n; j++){
+                if(j > i+1 && nums[j] == nums[j-1]){
+                    continue;
+                }
+                // two pointers
+                int k = j+1;
+                int l = n-1;
+                while(k < l){
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if(sum > target){
+                        l--;
+                    } else if(sum < target){
+                        k++;
                     } else {
-                        set.add((long)nums[k]);
+                        List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                        result.add(temp); 
+                        k++;
+                        l--;
+                        //skipping duplicates
+                        while(k<l && nums[k] ==nums[k-1]){
+                            k++;
+                        }
+                        while(k<l && nums[l] == nums[l+1]){
+                            l--;
+                        }
                     }
                 }
             }
         }
-        for(List<Integer> list: quad){
-            result.add(list);
-        }
+
+
 
         return result;
     }
