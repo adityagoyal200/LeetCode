7dc5190f128d24f9575527b47d2463b01065db11
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-06-11 09:10:57 +0530

    Time: 22 ms (35.1%), Space: 52.4 MB (11.69%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,21 +1,22 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        if(nums == null || nums.length == 0){
+        if (nums == null || nums.length == 0) {
             return 0;
         }
+        
         HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
-        int prefixSum  = 0;
+        map.put(0, 1);  // Initialize map with prefix sum 0.
+        int prefixSum = 0;
         int count = 0;
 
-        for(int num: nums){
+        for (int num : nums) {
             prefixSum += num;
 
-            if(map.containsKey(prefixSum - goal)){
+            if (map.containsKey(prefixSum - goal)) {
                 count += map.get(prefixSum - goal);
             }
             
-            map.put(prefixSum, map.getOrDefault(prefixSum,0)+1);
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
 
         return count;
