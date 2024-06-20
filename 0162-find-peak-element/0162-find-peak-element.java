010197ff0547cbab876604eb31d926d81309a3b7
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-06-20 21:08:04 +0530

    Time: 0 ms (100%), Space: 42.7 MB (9.15%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -23,9 +23,7 @@ class Solution {
             
             if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
                 return mid;
-            }
-
-            if(nums[mid] > nums[mid-1]){
+            }else if(nums[mid] > nums[mid-1]){
                 low = mid+1;
             } else if(nums[mid] > nums[mid+1]){
                 high = mid-1;
