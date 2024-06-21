44f5b24df37a83d46ea73ab2c06525bcebc236ad
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-06-21 20:01:14 +0530

    Time: 11 ms (18.38%), Space: 46.6 MB (31.93%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) {
+        if(intervals == null || intervals.length == 0){
             return new int[0][0];
         }
-        int n = intervals.length;
-        Arrays.sort(intervals,(int[]a,int[]b)->a[0]-b[0]);
-        
+
+        Arrays.sort(intervals,(a,b) ->a[0] -b[0]);
         List<int[]> ans = new ArrayList<>();
 
-        int start = intervals[0][0];
+        int start =  intervals[0][0];
         int end = intervals[0][1];
 
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i<intervals.length; i++){
             if(intervals[i][0] <= end){
                 end = Math.max(intervals[i][1],end);
             } else {
@@ -21,8 +20,7 @@ class Solution {
             }
         }
         ans.add(new int[]{start,end});
-        
+
         return ans.toArray(new int[ans.size()][]);
     }
 }
-
