9fac40dd64d5025d088e470014bf3f1b713df8d6
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-07-01 11:23:52 +0530

    Time: 0 ms (100%), Space: 44 MB (96.1%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -3,23 +3,24 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        
-        int i = 1; 
-        int count = 1; 
+
+        int i = 0;
+        int cnt = 1;
         
         for (int j = 1; j < nums.length; j++) {
-            if (nums[j] == nums[j - 1]) {
-                count++;
+            if (nums[i] == nums[j]) {
+                if (cnt < 2) {
+                    i++;
+                    nums[i] = nums[j];
+                    cnt++;
+                }
             } else {
-                count = 1;
-            }
-            
-            if (count <= 2) {
-                nums[i] = nums[j];
                 i++;
+                nums[i] = nums[j];
+                cnt = 1;
             }
         }
         
-        return i;
+        return i + 1;
     }
 }
