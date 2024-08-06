92bc8736b5fac7b442f8fe876d010fa3e994bc51
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-08-06 10:02:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,35 +3,34 @@ class Solution {
         if(nums == null || nums.length == 0){
             return -1;
         }
-        if(nums.length == 1){
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        if(nums.length == 1) {
             return 0;
         }
-
-        if(nums[0] > nums[1]){
+        if(nums[0] > nums[1]) {
             return 0;
         }
-        
-        if(nums[nums.length-1] > nums[nums.length -2]){
-            return nums.length-1;
+        if(nums[high] > nums[high-1]) {
+            return high;
         }
-
-        int low = 1;
-        int high = nums.length -2;
 
         while(low <= high){
-            int mid = low + (high - low)/2;
-            
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
-                return mid;
-            }else if(nums[mid] > nums[mid-1]){
-                low = mid+1;
-            } else if(nums[mid] > nums[mid+1]){
-                high = mid-1;
-            } else {
-                low = mid+1;
+            int mid = low + (high - low) / 2;
+
+            if(mid > 0 && mid < nums.length - 1) {
+                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                    return mid;
+                } else if(nums[mid-1] > nums[mid]){
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             }
         }
-        
+
         return -1;
     }
 }
