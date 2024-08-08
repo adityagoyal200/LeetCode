a1aa971dd0b10b9a23d365ef70082080748ba2f0
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-08-09 01:42:10 +0530

    Time: 1 ms (99.53%), Space: 56.8 MB (80.08%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,20 +3,20 @@ class Solution {
         if(nums == null || nums.length == 0){
             return -1;
         }
-        int sum  = 0;
+
+        int sum = 0;
         int max = Integer.MIN_VALUE;
-        // int ansStart = -1;
-        // int ansEnd = -1;
-        for(int i = 0 ;i < nums.length; i++){
+
+        for(int i = 0;i < nums.length; i++){
             sum += nums[i];
+        
             if(sum > max){
                 max = sum;
-                // ansEnd = i;
             }
+
             if(sum < 0){
-                sum = 0;
-                // ansStart = i;
-            }
+                sum  = 0;
+            } 
         }
 
         return max;
