ab53abd158b390c60dfc65fce6c6d6e693cd2e31
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-08-09 22:36:19 +0530

    Time: 4 ms (57.97%), Space: 39.8 MB (100%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String countAndSay(int n) {
+    String res = "1";
+    while(--n > 0){
+        char same = res.charAt(0);
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < res.length(); i++){
+            if(res.charAt(i) == same){
+                count++;
+            }
+            else{
+                sb.append(count);
+                sb.append(same);
+                same = res.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(same);
+        res = sb.toString();
+    }
+    return res;
+}
+}
