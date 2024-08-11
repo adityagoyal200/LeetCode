77c5f99bb2e6012991be9d3b51315b057183965b
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-08-11 22:46:10 +0530

    Time: 2 ms (78.24%), Space: 44.6 MB (85.22%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,34 +1,36 @@
 class Solution {
-	List<List<Integer>> result;
-	boolean[] used;
-	public List<List<Integer>> permuteUnique(int[] nums) {
-   	 	result = new ArrayList<>();
-		if(nums == null || nums.length == 0){
-	return result;
-}
-Arrays.sort(nums);
-used = new boolean[nums.length];
-backtrack(nums,0,new ArrayList<>());
+    List<List<Integer>> result;
+    boolean[] used;
 
-return result;
-}
-	private void backtrack(int[] nums, int index, List<Integer> list){
-	
-	// base
-	if(list.size() ==  nums.length){
-	result.add(new ArrayList<>(list));
-	return;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+        used = new boolean[nums.length];
+        backtrack(nums, 0, new ArrayList<>());
+
+        return result;
     }
-	// logic
-		for(int i = 0; i < nums.length; i++){
+
+    private void backtrack(int[] nums, int index, List<Integer> list) {
+
+        // base
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        // logic
+        for (int i = 0; i < nums.length; i++) {
             if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) {
-                        continue;
-                    }
+                continue;
+            }
             list.add(nums[i]);
 
             used[i] = true;
-            backtrack(nums,i+1,list);
-            list.remove(list.size() -1);
+            backtrack(nums, i + 1, list);
+            list.remove(list.size() - 1);
             used[i] = false;
         }
     }
