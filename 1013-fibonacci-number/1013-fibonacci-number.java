fe09fbfd2ad3bba3e4e10b0780f579195d435f33
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-08-19 17:26:28 +0530

    Time: 9 ms (24.42%), Space: 40.5 MB (24.04%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -6,8 +6,7 @@ class Solution {
         if(n == 1){
             return 1;
         }
-        else {
-            return fib(n - 1)+fib(n-2);
-        }
+        
+        return fib(n - 1)+fib(n-2);
     }
 }
