3e3d7198f50ebb87a995440ad5dac3799bc1e861
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-08-21 22:25:48 +0530

    Time: 0 ms (100%), Space: 41.6 MB (63.86%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -4,9 +4,11 @@ class Solution {
             return 0;
 
         int ans = 0, i = s.length() - 1;
+        
         while (i >= 0 && s.charAt(i) == ' '){
             i--;
         }
+
         while (i >= 0 && s.charAt(i) != ' ') {
             ans++;
             i--;
