5974978a27076523b2bbca99fc77645374ca66c7
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-08-28 15:57:30 +0530

    Time: 36 ms (19.96%), Space: 82.9 MB (69.21%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.java
+++ 2035-count-sub-islands/2035-count-sub-islands.java
@@ -1,21 +1,15 @@
 class Solution {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        boolean[][] islands = new boolean[grid1.length][grid1[0].length];
-
-        for (int i = 0; i < grid1.length; i++) {
-            for (int j = 0; j < grid1[0].length; j++) {
-                if (grid1[i][j] == 1 && !islands[i][j]) {
-                    helper(grid1, i, j, islands);
-                }
-            }
+        if(grid1.length == 0 || grid2.length == 0){
+            return 0;
         }
 
         boolean[][] visited = new boolean[grid2.length][grid2[0].length];
         int count = 0;
         for (int i = 0; i < grid2.length; i++) {
             for (int j = 0; j < grid2[0].length; j++) {
-                if (grid2[i][j] == 1 && !visited[i][j] && islands[i][j]) {
-                    count += helper2(grid2, i, j, islands, visited);
+                if(grid2[i][j] == 1 && grid1[i][j] == 1 && !visited[i][j]){
+                    count += helper(grid1,grid2,i,j,visited);
                 }
             }
         }
@@ -23,28 +17,7 @@ class Solution {
         return count;
     }
 
-    private void helper(int[][] grid, int row, int col, boolean[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        int[][] dirs = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
-        q.offer(new int[] { row, col });
-        visited[row][col] = true;
-
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            for (int[] dir : dirs) {
-                int newR = cell[0] + dir[0];
-                int newC = cell[1] + dir[1];
-
-                if (newR >= 0 && newR < grid.length && newC >= 0 &&
-                    newC < grid[0].length && grid[newR][newC] == 1 && !visited[newR][newC]) {
-                    q.offer(new int[] { newR, newC });
-                    visited[newR][newC] = true;
-                }
-            }
-        }
-    }
-
-    private int helper2(int[][] grid, int row, int col, boolean[][] islands, boolean[][] visited) {
+    private int helper(int[][] grid1, int[][] grid2, int row, int col,boolean[][] visited) {
         Queue<int[]> q = new LinkedList<>();
         int[][] dirs = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
         q.offer(new int[] { row, col });
@@ -57,13 +30,15 @@ class Solution {
                 int newR = cell[0] + dir[0];
                 int newC = cell[1] + dir[1];
 
-                if (newR >= 0 && newR < grid.length && newC >= 0 &&
-                        newC < grid[0].length && grid[newR][newC] == 1) {
+                if (newR >= 0 && newR < grid2.length && newC >= 0 &&
+                        newC < grid2[0].length && grid2[newR][newC] == 1) {
 
                     if (!visited[newR][newC]) {
-                        if (!islands[newR][newC]) {
+                        if (newR > grid1.length || newC > grid1[0].length || 
+                            grid1[newR][newC] != 1){
                             isSubIsland = false;
                         }
+                        
                         q.offer(new int[] { newR, newC });
                         visited[newR][newC] = true;
                     }
