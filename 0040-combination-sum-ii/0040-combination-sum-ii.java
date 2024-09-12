2bf61be264f764fc1239af064f27109a05ac9ed0
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-12 06:26:08 +0530

    Time: 5 ms (19.03%), Space: 43.3 MB (26.2%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,37 +1,48 @@
 class Solution {
-	List<List<Integer>> result;
-	int sum;
-	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-   	 	sum = 0;
-		result = new ArrayList<>();
-        
-		if(candidates == null || candidates.length == 0){
-	        return result;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        if(candidates == null || candidates.length == 0){
+            return new ArrayList<>();
         }
-
+        
         Arrays.sort(candidates);
-        backtrack(candidates,0,sum,target,new ArrayList<>());
+        List<List<Integer>> result = new ArrayList<>();
+        helper(candidates,0,0,target,new ArrayList<>(),result);
 
         return result;
-	}
-	public void backtrack(int[] nums, int index, int sum, int target, List<Integer> list){
-	//base
+    }
+    private void helper(int[] arr, int index, int sum,int target, List<Integer> list, List<List<Integer>> result){
+        //base
+        if(index == arr.length){
+            if(sum == target){
+                result.add(new ArrayList<>(list));
+                return;
+            }
+
+            return;
+        }
+
+        if(sum > target){
+            return;
+        }
+
         if(sum == target){
             result.add(new ArrayList<>(list));
             return;
         }
+
+
         //logic
-        for(int i = index; i < nums.length;i++){
-            if(i > index && nums[i] == nums[i-1]){
-                continue;
-            }
-            if(sum + nums[i] <= target){
-                sum+=nums[i];
-                list.add(nums[i]);
-                backtrack(nums,i+1,sum,target,list);
-                sum-=nums[i];
-                list.remove(list.size()-1);
-            }
+        
+
+        list.add(arr[index]);
+        helper(arr, index+1,sum+arr[index],target,list,result);
+        list.remove(list.size()-1);
+
+        while(index+1 < arr.length && arr[index] == arr[index+1]){
+            index++;
         }
+
+        helper(arr,index+1,sum,target,list,result);
+
     }
 }
