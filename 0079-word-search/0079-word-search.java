ed4baf6b942557505c81dc0c58d57254b50b6ba8
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-12 15:10:37 +0530

    Time: 140 ms (65.7%), Space: 41.5 MB (65.69%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,14 +1,12 @@
 class Solution {
-    int[][] dirs;
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
 
-        dirs = new int[][] {{1,0},{0,1},{-1,0},{0,-1}};
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (backtrack(board, word, i, j, 0)){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(dfs(board,word,i,j,0)){
                         return true;
                     }
                 }
@@ -17,35 +15,23 @@ class Solution {
 
         return false;
     }
-
-    private boolean backtrack(char[][] board, String word, int row, int col, int index) {
-        int m = board.length;
-        int n = board[0].length;
-
-        // base
-        if (index == word.length()) {
+    private boolean dfs(char[][] board, String word,int row,int col,int index){
+        if(index == word.length()){
             return true;
         }
 
-        if (row < 0 || row >= m || col < 0 || col >= n || board[row][col] != word.charAt(index)) {
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != word.charAt(index)){
             return false;
         }
 
+        char temp = board[row][col];
+        board[row][col] = '*';
 
-        if(board[row][col] == word.charAt(index)){
-            char temp = board[row][col];
-            board[row][col] = '#';
-            for(int[] dir: dirs){
-                int newR = row + dir[0];
-                int newC =  col + dir[1];
+        boolean found = dfs(board,word,row+1,col,index+1) || dfs(board,word,row-1,col,index+1) 
+        || dfs(board,word,row,col+1,index+1) || dfs(board,word,row,col-1,index+1);
 
-                if(backtrack(board,word,newR,newC,index+1)){
-                    board[row][col] = temp;
-                    return true;
-                }
-            }
-            board[row][col] = temp;
-        }
-        return false;
+        board[row][col] = temp;
+
+        return found;
     }
 }
