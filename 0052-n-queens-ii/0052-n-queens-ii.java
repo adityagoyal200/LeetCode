a5b3e3cfebad9efbe72bc7047427eb3cf63697d7
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-13 15:08:14 +0530

    Time: 3 ms (37.15%), Space: 40.7 MB (38.15%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,79 @@
+class Solution {
+    public int totalNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+
+        if(n <= 0 || n == 2 || n == 3){
+            return 0;
+        }
+
+        if(n == 1){
+            return 1;
+        }
+
+        int[][] board = new int[n][n];
+        helper(board,result,0,n);
+
+        return result.size();
+
+    }
+    private void helper(int[][] board,List<List<String>> result,int row,int n){
+        //base
+        if(row == n){
+            List<String> temp = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+               StringBuilder sb = new StringBuilder();
+                for(int j = 0; j < n; j++){
+                    if(board[i][j] == 1){
+                        sb.append("Q");
+                    } else {
+                        sb.append(".");
+                    }
+                }
+                temp.add(sb.toString());
+            }
+
+            result.add(temp);
+            return;
+        }
+
+        //logic
+        for(int col = 0; col <  n; col++){
+            if(isSafe(board,row,col,n)){
+                board[row][col] = 1;
+                helper(board,result,row+1,n);
+                board[row][col] = 0;
+            }
+        }
+    }
+    private boolean isSafe(int[][] board,int row,int col,int n){
+        for(int i = row; i >= 0; i--){
+            if(board[i][col] == 1){
+                return false;
+            }
+        }
+
+        int i = row,j=col;
+        while(i >=0 && j >=0){
+
+            if(board[i][j] == 1){
+                return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        i = row; j= col;
+        while(i >= 0 && j < n){
+
+            if(board[i][j] == 1){
+                return false;
+            }
+
+            i--;
+            j++;
+        }
+
+        return true;
+    }
+}
