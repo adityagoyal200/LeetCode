9507b43b54a5f84ea7cd144c2b8688301013d83e
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-13 10:06:37 +0530

    Time: 2 ms (100%), Space: 55.5 MB (85.32%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -5,19 +5,19 @@ class Solution {
         }
 
         int n = arr.length;
-        int[] prefixXor = new int[n + 1];
+        int[] prefixXor = new int[n+1];
 
         for (int i = 0; i < n; i++) {
-            prefixXor[i + 1] = prefixXor[i] ^ arr[i];
+            prefixXor[i+1] = prefixXor[i] ^ arr[i];
         }
 
         int[] result = new int[queries.length];
-        
+
         for (int i = 0; i < queries.length; i++) {
             int left = queries[i][0];
             int right = queries[i][1];
 
-            result[i] = prefixXor[right + 1] ^ prefixXor[left];
+            result[i] = prefixXor[right+1] ^ prefixXor[left];
         }
 
         return result;
