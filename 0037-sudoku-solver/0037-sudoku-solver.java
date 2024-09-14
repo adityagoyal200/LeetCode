1f669ae99025f370b18a9138966ee33b0a3d663b
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-14 15:44:40 +0530

    Time: 3 ms (96.15%), Space: 40.9 MB (69.75%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,80 +1,58 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        if (board == null || board.length == 0) {
-            return;
-        }
+    // Track numbers in rows, columns, and subgrids
+    private boolean[][] rows = new boolean[9][9];
+    private boolean[][] cols = new boolean[9][9];
+    private boolean[][] subgrids = new boolean[9][9];
 
+    public void solveSudoku(char[][] board) {
+        // Initialize tracking arrays based on the current state of the board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int subgridIndex = (i / 3) * 3 + j / 3;
+                    rows[i][num] = true;
+                    cols[j][num] = true;
+                    subgrids[subgridIndex][num] = true;
+                }
+            }
+        }
         solve(board);
     }
 
     private boolean solve(char[][] board) {
-        int n = board.length;
-        int row = -1;
-        int col = -1;
-        boolean emptyLeft = true;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    // Try placing digits 1-9
+                    for (char num = '1'; num <= '9'; num++) {
+                        int digit = num - '1';
+                        int subgridIndex = (i / 3) * 3 + j / 3;
 
-        // Find the first empty cell
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == '.') {  
-                    row = i;
-                    col = j;
-                    emptyLeft = false;
-                    break;
-                }
-            }
-            if (!emptyLeft) {
-                break;
-            }
-        }
+                        if (!rows[i][digit] && !cols[j][digit] && !subgrids[subgridIndex][digit]) {
+                            // Place the number and mark it
+                            board[i][j] = num;
+                            rows[i][digit] = true;
+                            cols[j][digit] = true;
+                            subgrids[subgridIndex][digit] = true;
 
-        if (emptyLeft) {
-            return true;  
-        }
-
-       
-        for (char num = '1'; num <= '9'; num++) {
-            if (isSafe(board, row, col, num)) {
-                board[row][col] = num;
-                if (solve(board)) {
-                    return true;
-                } else {
-                    board[row][col] = '.';
+                            // Recur with this choice
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                // Backtrack
+                                board[i][j] = '.';
+                                rows[i][digit] = false;
+                                cols[j][digit] = false;
+                                subgrids[subgridIndex][digit] = false;
+                            }
+                        }
+                    }
+                    return false;  // If no number fits, trigger backtracking
                 }
             }
         }
-
-        return false;
-    }
-
-    private boolean isSafe(char[][] board, int row, int col, char num) {
-        // Check row
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == num) {
-                return false;
-            }
-        }
-
-        // Check column
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][col] == num) {
-                return false;
-            }
-        }
-
-        // Check 3x3 subgrid
-        int sqrt = (int) Math.sqrt(board.length);
-        int rowStart = row - row % sqrt;
-        int colStart = col - col % sqrt;
-
-        for (int i = rowStart; i < rowStart + sqrt; i++) {
-            for (int j = colStart; j < colStart + sqrt; j++) {
-                if (board[i][j] == num) {
-                    return false;
-                }
-            }
-        }
-        
-        return true;
+        return true;  // Solution found
     }
 }
+
