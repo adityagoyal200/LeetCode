89f8ac26f66d91b36ae618a196bc7af0fec71572
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-14 14:44:07 +0530

    Time: 3 ms (15.69%), Space: 42.7 MB (15.08%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,31 +1,37 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null || s.isEmpty()) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
 
-        int i = 0, n = s.length(), sign = 1, result = 0;
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.trim());
 
-        while (i < n && s.charAt(i) == ' ') {
+        StringBuilder st = new StringBuilder();
+        int i = 0;
+        boolean neg = false;
+
+        if (i < sb.length() && sb.charAt(i) == '-') {
+            neg = true;
+            i++;
+        } else if (i < sb.length() && sb.charAt(i) == '+') {
             i++;
         }
 
-        if (i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
+        while (i < sb.length() && Character.isDigit(sb.charAt(i))) {
+            st.append(sb.charAt(i));
             i++;
         }
 
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
-
-            if (result > (Integer.MAX_VALUE - digit) / 10) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-
-            result = result * 10 + digit;
-            i++;
+        if (st.length() == 0) {
+            return 0;
         }
 
-        return result * sign;
+        try {
+            int num = Integer.parseInt(st.toString());
+            return neg ? -num : num;
+        } catch (NumberFormatException e) {
+            return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
     }
 }
