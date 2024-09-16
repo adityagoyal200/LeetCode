11bccb89aafcdb7206331d6d049100b8cb1ec8b7
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-16 16:02:12 +0530

    Time: 18 ms (60.25%), Space: 43.9 MB (51.44%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -4,25 +4,20 @@ class Solution {
             return new ArrayList<>();
         }
 
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
 
-        List<List<Integer>> result = new ArrayList<>();
-
         for(int i = 0; i < nums.length; i++){
-            if(i != 0 && nums[i-1] == nums[i]){
-                continue;
-            }
+            if(i != 0 && nums[i-1] == nums[i]) continue;
 
-            for(int j = i+1; j < nums.length; j++){
-               if(j > i+1 && nums[j] == nums[j-1]){
-                    continue;
-                }
+            for(int j = i+1; j< nums.length; j++){
+                if(j > i+1 && nums[j-1] == nums[j]) continue;
 
-                int k  = j+1;
+                int k = j+1;
                 int l = nums.length-1;
 
                 while(k < l){
-                    long sum = nums[i] + nums[j];
+                    long sum = nums[i]+nums[j];
                     sum += nums[k];
                     sum += nums[l];
 
@@ -30,17 +25,15 @@ class Solution {
                         result.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
                         k++;
                         l--;
-                        while(k<l && nums[k] == nums[k-1]){
-                            k++;
-                        }
-                        while(k<l && nums[l] == nums[l+1]){
-                            l--;
-                        }
-                    } else if(sum < target){
-                        k++;
-                    } else {
+
+                        while(k < l && nums[k] ==  nums[k-1]) k++;
+                        while(k < l && nums[l] ==  nums[l+1]) l--;
+                        
+                    } else if(sum > target){
                         l--;
-                    }
+                    } else {
+                        k++;
+                    }            
                 }
             }
         }
