928b37b93b8a2f2a7732d818af00bb4416aca785
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-24 19:43:29 +0530

    Time: 2 ms (67.88%), Space: 40.8 MB (34.73%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -4,7 +4,6 @@ class Solution {
         if(n <= 0 || n == 2 || n == 3){
             return 0;
         }
-        
         if(n == 1){
             return 1;
         }
@@ -16,23 +15,21 @@ class Solution {
         return count;
     }
     private void solve(int[][] grid,int row,int n){
-        //base
         if(row == n){
             count++;
             return;
         }
 
-        //logic
         for(int col = 0; col < n; col++){
             if(isSafe(grid,row,col)){
-                grid[row][col] = 1; 
+                grid[row][col] = 1;
                 solve(grid,row+1,n);
                 grid[row][col] = 0;
             }
         }
     }
-    private boolean isSafe(int[][] grid,int row,int col){
-        //check row
+
+    private boolean isSafe(int[][] grid, int row, int col){
         for(int i = row; i >= 0; i--){
             if(grid[i][col] == 1){
                 return false;
@@ -52,6 +49,7 @@ class Solution {
 
         i = row;
         j = col;
+
         while(i >= 0 && j < grid.length){
             if(grid[i][j] == 1){
                 return false;
