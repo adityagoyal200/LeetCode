123afff16b4bfc724c622c0f36a616a4c4d6173d
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-09-29 17:37:51 +0530

    Time: 48 ms (100%), Space: 57.9 MB (100%) - LeetHub

--- 3510-maximize-the-total-height-of-unique-towers/3510-maximize-the-total-height-of-unique-towers.java
+++ 3510-maximize-the-total-height-of-unique-towers/3510-maximize-the-total-height-of-unique-towers.java
@@ -1,22 +1,18 @@
 class Solution {
     public long maximumTotalSum(int[] maximumHeight) {
-        int n = maximumHeight.length;
         Arrays.sort(maximumHeight);
-        
+
         long sum = 0;
-        int lastH= Integer.MAX_VALUE;
-        
-        for (int i = n - 1; i >= 0; i--) {
-            int currH = Math.min(maximumHeight[i], lastH- 1);
-            
-            if (currH < 1) {
+        int lastHeight = Integer.MAX_VALUE;
+
+        for (int i = maximumHeight.length - 1; i >= 0; i--) {
+            lastHeight = Math.min(maximumHeight[i], lastHeight - 1);
+            if (lastHeight < 1) {
                 return -1;
             }
-            
-            sum += currH;
-            lastH= currH;
+            sum += lastHeight;
         }
-        
+
         return sum;
     }
 }
