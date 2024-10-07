96abf8e0c12d44c38c29d4c7486e7e0e7087bcd9
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-10-07 14:50:06 +0530

    Time: 72 ms (80.99%), Space: 45 MB (92.94%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,48 +1,51 @@
 class Solution {
     public List<String> addOperators(String num, int target) {
-        if(num == null || num.length() == 0){
+        if (num == null || num.length() == 0) {
             return new ArrayList<>();
         }
 
         List<String> list = new ArrayList<>();
-        helper(num,target,0,0,0,new StringBuilder(),list);
-
+        helper(num, target, 0, 0, 0, new StringBuilder(), list);
         return list;
     }
-    private void helper(String num, int target,long soFar,long prev, int index,StringBuilder sb,List<String> list){
-        if(index == num.length()){
-            if(soFar == target){
+
+    private void helper(String num, int target, long soFar, long prev, int index, StringBuilder sb, List<String> list) {
+        if (index == num.length()) {
+            if (soFar == target) {
                 list.add(sb.toString());
-            } 
+            }
             return;
         }
 
-        //logic 
-        //+ - *
-        for(int i = index; i < num.length(); i++){
-            if(num.charAt(index) == '0' && i != index){
+        // Loop through each substring of the number
+        for (int i = index; i < num.length(); i++) {
+            // Skip leading zeros
+            if (num.charAt(index) == '0' && i != index) {
                 continue;
             }
-            long curr = Long.parseLong(num.substring(index,i+1));
+            long curr = Long.parseLong(num.substring(index, i + 1));
             int len = sb.length();
 
-            if(index == 0){
+            if (index == 0) {
                 sb.append(curr);
-                helper(num,target,curr,curr,i+1,sb,list);
+                helper(num, target, curr, curr, i + 1, sb, list);
                 sb.setLength(len);
             } else {
+                // Try addition
                 sb.append("+").append(curr);
-                helper(num,target,soFar+curr,curr,i+1,sb,list);
+                helper(num, target, soFar + curr, curr, i + 1, sb, list);
                 sb.setLength(len);
 
+                // Try subtraction
                 sb.append("-").append(curr);
-                helper(num,target,soFar-curr,curr,i+1,sb,list);
+                helper(num, target, soFar - curr, -curr, i + 1, sb, list);
                 sb.setLength(len);
-                
+
+                // Try multiplication
                 sb.append("*").append(curr);
-                helper(num,target,soFar - prev + (prev * curr),curr,i+1,sb,list);
-                sb.setLength(len); 
+                helper(num, target, soFar - prev + (prev * curr), prev * curr, i + 1, sb, list);
+                sb.setLength(len);
             }
-        } 
+        }
     }
 }
