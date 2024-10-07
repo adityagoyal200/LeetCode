8629f7b17d868f6f62de9c619d39254e5d2a2439
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-10-07 21:16:39 +0530

    Time: 3 ms (91.37%), Space: 42.8 MB (98.52%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,32 +1,17 @@
 class Solution {
     public int minLength(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        StringBuilder sb = new StringBuilder();
+        StringBuilder stack = new StringBuilder();
 
-        return helper(s, sb, true);
-    }
-
-    private int helper(String s, StringBuilder sb, boolean canRemove) {
-        if (!canRemove) {
-            return s.length();
-        }
-
-        int i = 0;
-        canRemove = false;
-        while (i < s.length()) {
-            if (i + 1 < s.length() && ((s.charAt(i) == 'A' && s.charAt(i + 1) == 'B') || (s.charAt(i) == 'C' && s.charAt(i + 1) == 'D'))) {
-                i += 2;
-                canRemove = true;
+        for (char c : s.toCharArray()) {
+            int length = stack.length();
+            if (length > 0 &&
+                ((stack.charAt(length - 1) == 'A' && c == 'B') || (stack.charAt(length - 1) == 'C' && c == 'D'))) {
+                stack.deleteCharAt(length - 1); 
             } else {
-                sb.append(s.charAt(i));
-                i++;
+                stack.append(c); 
             }
         }
 
-        s = sb.toString();
-        sb = new StringBuilder();
-        return helper(s, sb, canRemove); 
+        return stack.length();
     }
 }
