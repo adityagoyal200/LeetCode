fd3b0d6b0aeaa2480aba8e547ee027456fb32823
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-10-12 21:44:32 +0530

    Time: 34 ms (83.42%), Space: 80.4 MB (38.59%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
@@ -1,30 +1,36 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        if(intervals == null || intervals.length == 0){
+        if (intervals == null || intervals.length == 0) {
             return 0;
         }
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int count = 0;
 
-        for(int[] interval: intervals){
-            int start = interval[0];
-            int end = interval[1];
+        int n = intervals.length;
+        int[] starts = new int[n];
+        int[] ends = new int[n];
 
-            if(!pq.isEmpty()){
-                if(start > pq.peek()){
-                    pq.poll();
-                    pq.offer(end);
-                } else {
-                    count++;
-                    pq.offer(end);
-                }
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+
+        int maxGroups = 0;
+        int activeGroups = 0;
+        int i = 0, j = 0;
+
+        while (i < n) {
+            if (starts[i] <= ends[j]) {
+                activeGroups++;
+                maxGroups = Math.max(maxGroups, activeGroups);
+                i++;
             } else {
-                count++;
-                pq.offer(end);
+                activeGroups--;
+                j++;
             }
         }
 
-        return count;
+        return maxGroups;
     }
 }
