0ae145651e71a43c15070840a315daf7d8f7c605
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-10-12 21:42:52 +0530

    Time: 95 ms (55.44%), Space: 76.2 MB (61.14%) - LeetHub

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minGroups(int[][] intervals) {
+        if(intervals == null || intervals.length == 0){
+            return 0;
+        }
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int count = 0;
+
+        for(int[] interval: intervals){
+            int start = interval[0];
+            int end = interval[1];
+
+            if(!pq.isEmpty()){
+                if(start > pq.peek()){
+                    pq.poll();
+                    pq.offer(end);
+                } else {
+                    count++;
+                    pq.offer(end);
+                }
+            } else {
+                count++;
+                pq.offer(end);
+            }
+        }
+
+        return count;
+    }
+}
