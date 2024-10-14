f9542dbd026f466a0fe4e12098b9784f39e53270
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-10-14 22:21:33 +0530

    Time: 113 ms (99.05%), Space: 63.1 MB (37.91%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
@@ -5,19 +5,17 @@ class Solution {
         }
 
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-
-        for (int x : nums){
-            pq.offer(x);
+        for (int i : nums) {
+            pq.add(i);
         }
 
-        long score = 0;
-        while (!pq.isEmpty() && k > 0) {
-            score += pq.peek();
-            int x = (int) Math.ceil(pq.poll() / 3.0);
-            pq.add(x);
-            k--;
+        long ans = 0;
+        while (k-- > 0) {
+            int n = pq.poll();
+            ans += n;
+            pq.offer((n + 2) / 3);
         }
         
-        return score;
+        return ans;
     }
 }
