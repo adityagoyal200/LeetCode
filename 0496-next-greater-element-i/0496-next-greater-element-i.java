5d22b96ec98fd1b11202d9c5491b42115e46376c
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-10-17 00:58:52 +0530

    Time: 4 ms (43.23%), Space: 43.8 MB (41.12%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        if(nums1 == null || nums1.length == 0){
-            return new int[]{};
+        if (nums1 == null || nums1.length == 0) {
+            return new int[] {};
         }
         int[] nge = new int[nums1.length];
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i< nums1.length; i++){
-            map.put(nums1[i],i); // 4,0/ 1,1 /2,2
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], i); // 4,0/ 1,1 /2,2
         }
         Stack<Integer> st = new Stack<>();
-        int i = nums2.length-1;
+        int i = nums2.length - 1;
 
-        while(i >= 0){ //4
-            if(map.containsKey(nums2[i])){ //
-                if(st.isEmpty()){
-                    nge[map.get(nums2[i])] = -1;//[-1,,-1]
+        while (i >= 0) { // 4
+            if (map.containsKey(nums2[i])) { //
+                if (st.isEmpty()) {
+                    nge[map.get(nums2[i])] = -1;// [-1,,-1]
                 } else {
-                    if(st.peek() < nums2[i]){
-                        while(!st.isEmpty() && st.peek() < nums2[i]){
+                    if (st.peek() < nums2[i]) {
+                        while (!st.isEmpty() && st.peek() < nums2[i]) {
                             st.pop();
                         }
-                        nge[map.get(nums2[i])] = (st.isEmpty()) ? -1: st.peek();
+                        nge[map.get(nums2[i])] = (st.isEmpty()) ? -1 : st.peek();
                     } else {
                         nge[map.get(nums2[i])] = st.peek();
                     }
