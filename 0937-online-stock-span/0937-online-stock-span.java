db9b042ea41d47a6c47bf6cfd04841dd5808ceed
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-10-21 01:25:01 +0530

    Time: 31 ms (60.24%), Space: 55.2 MB (50.98%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -7,15 +7,12 @@ class StockSpanner {
     
     public int next(int price) {
         int count = 1;
-        if(st.isEmpty()){
-            st.push(new int[]{price,1});
-            return count;
-        } else {
-            while(!st.isEmpty() && st.peek()[0] <= price){
-                count += st.pop()[1];
-            }
-            st.push(new int[]{price,count});
+        
+        while(!st.isEmpty() && st.peek()[0] <= price){
+            count += st.pop()[1];
         }
+
+        st.push(new int[]{price,count});
 
         return count;
     }
