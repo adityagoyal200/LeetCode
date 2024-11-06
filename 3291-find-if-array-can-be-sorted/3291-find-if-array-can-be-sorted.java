ec4663a7945576cc9fe41c4d6cc80d8e644974b6
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-11-06 17:09:22 +0530

    Time: 1 ms (100%), Space: 44.3 MB (48.43%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -1,25 +1,24 @@
 class Solution {
-    public static boolean canSortArray(final int[] nums) {
-        short prevMax = 0, currMin = 0, currMax = 0;
+    public static boolean canSortArray(int[] nums) {
+        int prevMax = 0, currMin = 0, currMax = 0;
         byte prevCount = 0;
 
-        for (final int num : nums) {
-            final byte currCount = (byte) Integer.bitCount(num);
+        for (int num : nums) {
+            byte currCount = (byte) Integer.bitCount(num);
 
             if (prevCount == currCount) {
-                currMin = (short) Math.min(currMin, num);
-                currMax = (short) Math.max(currMax, num);
+                currMin = Math.min(currMin, num);
+                currMax = Math.max(currMax, num);
             } else {
                 if (currMin < prevMax) {
                     return false;
                 }
-
                 prevMax = currMax;
-                currMin = currMax = (short) num;
+                currMin = currMax = num;
                 prevCount = currCount;
             }
         }
-
+        
         return currMin >= prevMax;
     }
 }
