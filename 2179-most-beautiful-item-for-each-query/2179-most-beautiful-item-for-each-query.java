f05aca426d4acb01dea6d600112003d09c04436b
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-11-12 17:57:17 +0530

    Time: 44 ms (82.61%), Space: 81.5 MB (26.63%) - LeetHub

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.java
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.java
@@ -1,37 +1,36 @@
 public class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
-        int maxI = Integer.MAX_VALUE;
-        List<int[]> res = new ArrayList<>();
-        res.add(new int[] {0, 0, maxI});
+        if (items == null || items.length == 0) {
+            return new int[] {};
+        }
 
-        Arrays.sort(items, (a,b)-> a[0]-b[0]);
+        Arrays.sort(items, (a, b) -> Integer.compare(a[0], b[0]));
 
-        for (int[] item : items) {
-            int price = item[0];
-            int beauty = item[1];
-            int[] lastBracket = res.get(res.size() - 1);
+        int n = items.length;
+        int[] maxBeautyAtPrice = new int[n];
+        maxBeautyAtPrice[0] = items[0][1];  
 
-            if (beauty > lastBracket[1]) {
-                lastBracket[2] = price;
-                res.add(new int[] {price, beauty, maxI});
-            }
+        for (int i = 1; i < n; i++) {
+            maxBeautyAtPrice[i] = Math.max(maxBeautyAtPrice[i - 1], items[i][1]);
         }
 
         int[] ans = new int[queries.length];
-        for (int j = 0; j < queries.length; j++) {
-            int x = queries[j];
-            int left = 0, right = res.size() - 1, bestBeauty = 0;
+
+        for (int i = 0; i < queries.length; i++) {
+            int x = queries[i];
+            int left = 0, right = n - 1, bestB = 0;
+
             while (left <= right) {
                 int mid = left + (right - left) / 2;
-                if (res.get(mid)[0] <= x) {
-                    bestBeauty = Math.max(bestBeauty,res.get(mid)[1]);
+                if (items[mid][0] <= x) {
+                    bestB = maxBeautyAtPrice[mid];
                     left = mid + 1; 
                 } else {
                     right = mid - 1; 
                 }
             }
-            
-            ans[j] = bestBeauty;
+
+            ans[i] = bestB;
         }
 
         return ans;
