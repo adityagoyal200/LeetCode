b0fc1ef48639220b791e16778cc139d6e5764274
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-11-13 10:07:51 +0530

    Time: 3 ms (47.2%), Space: 45.3 MB (83.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0; 
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+}
