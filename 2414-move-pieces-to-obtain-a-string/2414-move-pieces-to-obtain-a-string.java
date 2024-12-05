50f406017036c6243a151f5c0e4a49ea98fb00c4
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-05 20:42:43 +0530

    Time: 44 ms (24.12%), Space: 45.9 MB (18.75%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -1,39 +1,33 @@
 class Solution {
     public boolean canChange(String start, String target) {
         int n = start.length();
-        if (start.replace("_", "").equals(target.replace("_", ""))) {
-            int i = 0;
-            int j = 0;
 
-            while (i < n && j < n) {
-                while (i < n && start.charAt(i) == '_') {
-                    i++;
-                }
-
-                while (j < n && target.charAt(j) == '_') {
-                    j++;
-                }
-
-                if(i == n || j==n) return i==n && j==n;
-
-                if (i < n && j < n && start.charAt(i) != target.charAt(j)) {
-                    return false;
-                }
-
-                if (i < n && j < n && start.charAt(i) == 'L' && i < j) {
-                    return false;
-                }
-                if (i < n && j < n && start.charAt(i) == 'R' && i > j) {
-                    return false;
-                }
-
-                i++;
-                j++;
-            }
-            
-            return true;
+        if (!start.replace("_", "").equals(target.replace("_", ""))) {
+            return false;
         }
-        
-        return false;
+
+        int i = 0, j = 0;
+
+        while (i < n && j < n) {
+            while (i < n && start.charAt(i) == '_') i++;
+            while (j < n && target.charAt(j) == '_') j++;
+
+            if (i == n || j == n) {
+                return i == j;
+            }
+
+            if (start.charAt(i) != target.charAt(j)) {
+                return false;
+            }
+
+            if ((start.charAt(i) == 'L' && i < j) || (start.charAt(i) == 'R' && i > j)) {
+                return false;
+            }
+
+            i++;
+            j++;
+        }
+
+        return true;
     }
 }
