702a71e39544d120e6853f1caa7db5613069e39d
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-11 21:09:51 +0530

    Time: 36 ms (87.36%), Space: 60.4 MB (41.09%) - LeetHub

---
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        
+        Arrays.sort(nums);
+        int i = 0, j, n = nums.length;
+
+        for (j = 0; j < n; ++j) {
+            if (nums[j] - nums[i] > k * 2) {
+                i++;
+            }
+        }
+
+        return j - i;
+    }
+}
