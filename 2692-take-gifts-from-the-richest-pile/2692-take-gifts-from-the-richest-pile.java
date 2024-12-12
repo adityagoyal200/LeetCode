c1c7b341f552fe9385c1a8cdedcd71b1b7f3bcc8
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-12 20:35:26 +0530

    Time: 5 ms (89.83%), Space: 42.8 MB (16.45%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,26 +1,22 @@
 class Solution {
-    
     public long pickGifts(int[] gifts, int k) {
-        if(gifts == null || gifts.length == 0){
+        if (gifts == null || gifts.length == 0) {
             return 0;
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
-        for(int i = 0; i < gifts.length; i++){
-            pq.add(new int[]{i,gifts[i]});
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int gift : gifts) {
+            maxHeap.offer(gift);
         }
 
-        for(int i = 0; i < k; i++){
-            int[] currGift = pq.poll();
-            int val = (int)(Math.sqrt(currGift[1]));
-            int index = currGift[0];
-            gifts[index] = val;
-            pq.offer(new int[]{index,val}); 
+        for (int i = 0; i < k; i++) {
+            int largest = maxHeap.poll(); 
+            maxHeap.offer((int) Math.sqrt(largest)); 
         }
 
         long sum = 0;
-        for(int gift: gifts){
-            sum += gift;
+        while (!maxHeap.isEmpty()) {
+            sum += maxHeap.poll();
         }
 
         return sum;
