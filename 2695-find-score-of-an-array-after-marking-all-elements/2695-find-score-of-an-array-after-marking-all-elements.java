4790de93e9be8ec58045dbfee4cf2b2fcddb1042
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-13 21:27:39 +0530

    Time: 222 ms (24.51%), Space: 60.8 MB (36.11%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -0,0 +1,36 @@
+class Solution {
+    public long findScore(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]); 
+            }
+            return Integer.compare(a[0], b[0]); 
+        });
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            pq.offer(new int[]{nums[i], i});
+        }
+
+        long score = 0;
+        while (!pq.isEmpty()) {
+            while (!pq.isEmpty() && set.contains(pq.peek()[1])) {
+                pq.poll();
+            }
+            if (pq.isEmpty()) break; 
+
+            int[] val = pq.poll();
+            score += val[0];
+            int idx = val[1];
+
+            if (idx - 1 >= 0) set.add(idx - 1);
+            if (idx + 1 < nums.length) set.add(idx + 1);
+        }
+
+        return score;
+    }
+}
