82f401f1a494f0d8d385008f6cd617aa83edd0a6
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-13 21:28:25 +0530

    Time: 232 ms (17.65%), Space: 61.7 MB (18.98%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -1,15 +1,12 @@
+import java.util.*;
+
 class Solution {
     public long findScore(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        if (nums == null || nums.length == 0) return 0;
 
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if (a[0] == b[0]) {
-                return Integer.compare(a[1], b[1]); 
-            }
-            return Integer.compare(a[0], b[0]); 
-        });
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> 
+            a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0])
+        );
 
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
@@ -21,14 +18,14 @@ class Solution {
             while (!pq.isEmpty() && set.contains(pq.peek()[1])) {
                 pq.poll();
             }
-            if (pq.isEmpty()) break; 
+            if (pq.isEmpty()) break;
 
             int[] val = pq.poll();
             score += val[0];
             int idx = val[1];
 
-            set.add(idx - 1);
-            set.add(idx + 1);
+            if (idx - 1 >= 0) set.add(idx - 1);
+            if (idx + 1 < nums.length) set.add(idx + 1);
         }
 
         return score;
