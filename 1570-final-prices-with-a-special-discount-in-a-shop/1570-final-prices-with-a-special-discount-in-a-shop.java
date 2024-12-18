0eb47b14bfc7c6b2e600d893f254421775b3e234
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-18 22:32:16 +0530

    Time: 4 ms (40.47%), Space: 43.9 MB (31.07%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        if(prices == null || prices.length == 0){
+            return new int[]{};
+        }
+
+        int[] result = new int[prices.length];
+        result = findNse(prices);
+
+        for(int i = 0; i < result.length; i++){
+            if(result[i] == -1){
+                result[i] = prices[i];
+            } else {
+                result[i] = prices[i] - prices[result[i]];
+            }
+        }
+
+        return result;        
+    }
+    private int[] findNse(int[] arr){
+        int[] result = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i = arr.length-1; i >= 0; i--){
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+                st.pop();
+            }
+
+            result[i] = (st.isEmpty()) ? -1 : st.peek();
+            st.push(i);
+        }
+
+        return result;
+    }
+}
