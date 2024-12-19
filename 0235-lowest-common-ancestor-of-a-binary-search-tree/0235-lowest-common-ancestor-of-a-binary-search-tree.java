a270a746704a3b44118752f8734fa2031ad43c72
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-19 11:32:57 +0530

    Time: 5 ms (100%), Space: 44.8 MB (75.97%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,43 +1,30 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || p == root || q == root) {
-            return root;
+        if (root == null) {
+            return null;
         }
 
-        List<TreeNode> listP = new ArrayList<>();
-        List<TreeNode> listQ = new ArrayList<>();
-
-        findElement(root, p, listP);
-        findElement(root, q, listQ);
-
-        int i = 0;
-        for (; i < listP.size() && i < listQ.size() && listP.get(i) == listQ.get(i); ) {
-            i++;
-        }
-
-        return listP.get(i - 1);
-    }
-
-    private void findElement(TreeNode root, TreeNode target, List<TreeNode> path) {
-        while (root != null) {
-            path.add(root);
-            if (root.val == target.val) {
-                return;
-            } else if (root.val < target.val) {
-                root = root.right;
+        TreeNode temp = root;
+        while (temp != null) {
+            if (p.val < temp.val && q.val < temp.val) {
+                temp = temp.left;
+            } else if (p.val > temp.val && q.val > temp.val) {
+                temp = temp.right;
             } else {
-                root = root.left;
+                return temp;
             }
         }
+
+        return root;
     }
 }
