41a89c7d6a177f09aad2358a083db70e36f64144
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-20 11:15:30 +0530

    Time: 20 ms (44.99%), Space: 45.1 MB (48.27%) - LeetHub

---
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num: nums){
+            if(num % 2 == 0) map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        List<Integer>[] frequent = new List[nums.length+1];
+        for(int key: map.keySet()){
+            int val = map.get(key);
+            if(frequent[val] == null) frequent[val] = new ArrayList<>();
+            frequent[val].add(key);
+        }
+
+        for(int i = frequent.length-1; i >=0;i--){
+            if(frequent[i] != null){
+                List<Integer> list = frequent[i];
+                Collections.sort(list);
+                return list.get(0);
+            }
+        }
+
+        return -1;
+    }
+}
