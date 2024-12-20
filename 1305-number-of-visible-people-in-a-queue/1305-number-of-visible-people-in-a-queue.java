a622f6f7c700d534d843320f7aa89ec57584615e
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-21 04:42:54 +0530

    Time: 55 ms (37.01%), Space: 59.6 MB (67.63%) - LeetHub

--- 1305-number-of-visible-people-in-a-queue/1305-number-of-visible-people-in-a-queue.java
+++ 1305-number-of-visible-people-in-a-queue/1305-number-of-visible-people-in-a-queue.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] canSeePersonsCount(int[] heights) {
         Stack<Integer> st = new Stack();
+        int[] result = new int[heights.length];
 
         for (int i = heights.length-1; i >=0 ; i--){
-            int h = heights[i], count = 0;
-
-            while(!st.isEmpty() && st.peek() < h){
-                count++;
+            int cnt = 0;
+            while(!st.isEmpty() && heights[st.peek()] < heights[i]){
+                cnt++;
                 st.pop();
             }
 
-            heights[i] = count + (st.isEmpty() ? 0: 1);
-            st.push(h);
+            result[i] = cnt + (!st.isEmpty() ? 1 : 0);
+            st.push(i);
         }
-        return heights;
+
+        return result;
     }
 }
