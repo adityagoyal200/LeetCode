c6f714d14894700c45d46a1665c0e05c734be3d5
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-20 11:20:49 +0530

    Time: 13 ms (94.1%), Space: 45.3 MB (25.87%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.java
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -1,31 +1,20 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        if(nums == null || nums.length == 0){
-            return -1;
-        }
+        int result = -1,maxFreq = 0;
+        Map<Integer, Integer> fMap = new HashMap<>();
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num: nums){
-            if(num % 2 == 0) map.put(num,map.getOrDefault(num,0)+1);
-        }
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                int freq = fMap.getOrDefault(num, 0) + 1;
+                fMap.put(num, freq);
 
-        if(map.size() == 0) return -1;
-
-        List<Integer>[] frequent = new List[nums.length+1];
-        for(int key: map.keySet()){
-            int val = map.get(key);
-            if(frequent[val] == null) frequent[val] = new ArrayList<>();
-            frequent[val].add(key);
-        }
-
-        for(int i = frequent.length-1; i >=0;i--){
-            if(frequent[i] != null){
-                List<Integer> list = frequent[i];
-                Collections.sort(list);
-                return list.get(0);
+                if (freq > maxFreq || (freq == maxFreq && num < result)) {
+                   maxFreq = freq;
+                    result = num;
+                }
             }
         }
 
-        return -1;
+        return result;
     }
 }
