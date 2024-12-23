d43f73980e0a778b0be8eb35f63728e2312f8aec
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-23 22:15:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if(nums == null || nums.length == 0){
+        if (nums == null || nums.length < 3) {
             return new ArrayList<>();
         }
 
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
 
-        
-        for(int i = 0; i < nums.length; i++){
-            if (i != 0 && nums[i] == nums[i - 1]){ 
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            }
-            int j = i+1;
-            int k = nums.length -1;
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0){
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    result.add(list);
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) {
-                        j++;
-                    }
-                    while (j < k && nums[k] == nums[k + 1]){
-                        k--;
-                    }
-                } else if(sum > 0){
-                    k--;
-                } else {
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                while (j < k && nums[j] == nums[j + 1]) {
                     j++;
                 }
+                while (j < k && nums[k] == nums[k - 1]) {
+                    k--;
+                }
                 
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
             }
         }
 
