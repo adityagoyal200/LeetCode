c2366269b844841c77b6aa78c5399fe794af96bc
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-23 22:24:41 +0530

    Time: 19 ms (34.94%), Space: 43.9 MB (53.3%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,39 +1,34 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        if(nums == null || nums.length == 0){
+        if (nums == null || nums.length < 4) {
             return new ArrayList<>();
         }
 
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-
-        for(int i = 0; i < nums.length; i++){
-            if(i != 0 && nums[i-1] == nums[i]) continue;
-
-            for(int j = i+1; j< nums.length; j++){
-                if(j > i+1 && nums[j-1] == nums[j]) continue;
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j > i+1 && nums[j] == nums[j - 1]) continue;
 
                 int k = j+1;
-                int l = nums.length-1;
+                int l = nums.length -1;
 
                 while(k < l){
-                    long sum = nums[i]+nums[j];
+                    long sum = nums[i] +  nums[j];
                     sum += nums[k];
                     sum += nums[l];
-
-                    if(sum == target){
+                    if(sum ==  target){
                         result.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
                         k++;
                         l--;
-
-                        while(k < l && nums[k] ==  nums[k-1]) k++;
-                        while(k < l && nums[l] ==  nums[l+1]) l--;
-                        
-                    } else if(sum > target){
-                        l--;
-                    } else {
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;
+                    } else if(sum < target){
                         k++;
-                    }            
+                    } else {
+                        l--;
+                    }
                 }
             }
         }
