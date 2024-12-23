f4fd16c21f5bbf8dafc913c8535f930e7393f807
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-23 22:24:56 +0530

    Time: 19 ms (34.94%), Space: 43.9 MB (65.36%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -7,24 +7,28 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
             for (int j = i + 1; j < nums.length; j++) {
-                if (j > i+1 && nums[j] == nums[j - 1]) continue;
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
 
-                int k = j+1;
-                int l = nums.length -1;
+                int k = j + 1;
+                int l = nums.length - 1;
 
-                while(k < l){
-                    long sum = nums[i] +  nums[j];
+                while (k < l) {
+                    long sum = nums[i] + nums[j];
                     sum += nums[k];
                     sum += nums[l];
-                    if(sum ==  target){
-                        result.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         k++;
                         l--;
-                        while(k < l && nums[k] == nums[k-1]) k++;
-                        while(k < l && nums[l] == nums[l+1]) l--;
-                    } else if(sum < target){
+                        while (k < l && nums[k] == nums[k - 1])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+                    } else if (sum < target) {
                         k++;
                     } else {
                         l--;
