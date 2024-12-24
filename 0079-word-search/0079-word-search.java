2bdb229e2f6e94b4ef15d4ff50e53ab8d6cb5438
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-24 22:13:05 +0530

    Time: 143 ms (61.76%), Space: 41.6 MB (61.84%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,43 +1,35 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        if(board == null || board.length == 0){
+        if(board == null || board.length == 0 || word.length() == 0){
             return false;
         }
 
-        
-
-        for(int i = 0; i < board.length; i++){
+        for(int i = 0;i<board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == word.charAt(0)){
-                    if(helper(board,i,j,0,word)){
-                        return true;
-                    }
+                    if(dfs(board,word,i,j,0)) return true;
                 }
             }
         }
 
         return false;
     }
-    private boolean helper(char[][] board,int row, int col, int index, String word){
-        //base
+    private boolean dfs(char[][] board, String word, int row, int col,int index){
         if(index == word.length()){
             return true;
         }
 
-        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != word.charAt(index)){
-            return false;
-        }
-        
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length 
+        || board[row][col] != word.charAt(index)) return false;
+
         char temp = board[row][col];
         board[row][col] = '#';
-
-        boolean found = helper(board,row+1,col,index+1,word)
-                        || helper(board,row-1,col,index+1,word) 
-                        || helper(board,row,col+1,index+1,word)
-                        || helper(board,row,col-1,index+1,word);
+        boolean found = dfs(board,word,row+1,col,index+1) 
+        || dfs(board,word,row-1,col,index+1)
+        || dfs(board,word,row,col+1,index+1)
+        || dfs(board,word,row,col-1,index+1);
 
         board[row][col] = temp;
-
         return found;
     }
 }
