36cc2c1116a1dfeefc416bd8cf6948117c5612b5
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-24 23:39:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (63.28%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,40 +1,40 @@
 class Solution {
+    HashMap<Integer, List<String>> map;
+
     public List<String> letterCombinations(String digits) {
-        if(digits == null || digits.length() == 0){
+        if (digits == null || digits.length() == 0) {
             return new ArrayList<>();
         }
 
         List<String> result = new ArrayList<>();
-        HashMap<Integer,List<String>> map = new HashMap<>();
+        map = new HashMap<>();
 
         map.put(2, Arrays.asList("a", "b", "c"));
-    	map.put(3, Arrays.asList("d", "e", "f"));
-    	map.put(4, Arrays.asList("g", "h", "i"));
-    	map.put(5, Arrays.asList("j", "k", "l"));
-    	map.put(6, Arrays.asList("m", "n", "o"));
-    	map.put(7, Arrays.asList("p", "q", "r", "s"));
-    	map.put(8, Arrays.asList("t", "u", "v"));
-    	map.put(9, Arrays.asList("w", "x", "y", "z"));
+        map.put(3, Arrays.asList("d", "e", "f"));
+        map.put(4, Arrays.asList("g", "h", "i"));
+        map.put(5, Arrays.asList("j", "k", "l"));
+        map.put(6, Arrays.asList("m", "n", "o"));
+        map.put(7, Arrays.asList("p", "q", "r", "s"));
+        map.put(8, Arrays.asList("t", "u", "v"));
+        map.put(9, Arrays.asList("w", "x", "y", "z"));
 
-        helper(digits,0,new StringBuilder(),result,map);
+        helper(digits, 0, new StringBuilder(), result);
 
         return result;
     }
-    private void helper(String digits,int index,StringBuilder sb,List<String> list,Map<Integer,List<String>> map){
 
-        //base
-        if(index == digits.length()){
-            list.add(sb.toString());
+    private void helper(String digits, int start, StringBuilder sb, List<String> result) {
+        if (start == digits.length()) {
+            result.add(sb.toString());
             return;
         }
 
-        //logic
-        List<String> temp = map.get(digits.charAt(index)-'0');
-        for(String s: temp){
+        List<String> list = map.get(digits.charAt(start) - '0');
+        for (String s : list) {
             sb.append(s);
-            helper(digits,index+1,sb,list,map);
-            sb.deleteCharAt(sb.length()-1);
-        } 
-        
+            helper(digits, start + 1, sb, result);
+            sb.setLength(sb.length() - 1);
+        }
+
     }
 }
