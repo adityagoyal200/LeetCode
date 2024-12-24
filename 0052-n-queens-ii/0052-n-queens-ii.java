c0d396a0ab6c52dd6aabe0dd638743927216623c
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-24 22:34:56 +0530

    Time: 1 ms (83.38%), Space: 40.4 MB (61.34%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,59 +1,48 @@
 class Solution {
-    int count;
+    int cnt;
     public int totalNQueens(int n) {
         if(n <= 0 || n == 2 || n == 3){
             return 0;
         }
-        if(n == 1){
-            return 1;
-        }
 
-        count = 0;
-        int[][] grid = new int[n][n];
-        solve(grid,0,n);
+        cnt = 0;
+        int[][] board = new int[n][n];
+        helper(board,0,n);
 
-        return count;
+        return cnt;
+        
     }
-    private void solve(int[][] grid,int row,int n){
+    private void helper(int[][] board, int row, int n){
         if(row == n){
-            count++;
+            cnt++;
             return;
         }
 
         for(int col = 0; col < n; col++){
-            if(isSafe(grid,row,col)){
-                grid[row][col] = 1;
-                solve(grid,row+1,n);
-                grid[row][col] = 0;
+            if(isSafe(board,row,col,n)){
+                board[row][col] = 1;
+                helper(board,row+1,n);
+                board[row][col] = 0;
             }
         }
     }
-
-    private boolean isSafe(int[][] grid, int row, int col){
+    private boolean isSafe(int[][] board, int row, int col, int n){
         for(int i = row; i >= 0; i--){
-            if(grid[i][col] == 1){
-                return false;
-            }
+            if(board[i][col] == 1) return false;
         }
 
-        int i = row;
-        int j = col;
+        int i = row; int j = col;
 
         while(i >= 0 && j >= 0){
-            if(grid[i][j] == 1){
-                return false;
-            }
+            if(board[i][j] == 1) return false;
             i--;
             j--;
         }
 
-        i = row;
-        j = col;
+        i = row; j = col;
 
-        while(i >= 0 && j < grid.length){
-            if(grid[i][j] == 1){
-                return false;
-            }
+        while(i >= 0 && j < n){
+            if(board[i][j] == 1) return false;
             i--;
             j++;
         }
