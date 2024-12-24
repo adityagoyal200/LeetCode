30b827fa3537f0e6fb55820d8202bec8924bb13d
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-24 20:38:35 +0530

    Time: 14 ms (62.6%), Space: 46.8 MB (41.74%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,51 +1,57 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
+        if(matrix == null || matrix.length == 0){
             return 0;
         }
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        int[][] prefixArr = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix[0].length; i++) {
+        int[][] prefixArray = new int[m][n];
+        for(int i = 0; i < n; i++){
             int sum = 0;
-            for (int j = 0; j < matrix.length; j++) {
-                if (matrix[j][i] == '1') {
-                    sum++;
-                    prefixArr[j][i] = sum;
-                } else {
+            for(int j = 0; j < m;j++){
+                if(matrix[j][i] == '0'){
                     sum = 0;
-                    prefixArr[j][i] = sum;
+                    prefixArray[j][i] = 0;
+                } else {
+                    sum++;
+                    prefixArray[j][i] = sum;
                 }
             }
         }
 
-        int largest = 0;
+        int maximal = 0;
 
-        for (int i = 0; i < matrix.length; i++) {
-            int area = findArea(prefixArr, i);
-            largest = Math.max(area, largest);
+        for(int i = 0; i<m; i++){
+            int maxArea = findMaxArea(prefixArray,i);
+            maximal = Math.max(maximal,maxArea);
         }
 
-        return largest;
+        return maximal;
     }
-
-    private int findArea(int[][] arr, int row) {
+    private int findMaxArea(int[][] arr,int row){
         Stack<Integer> st = new Stack<>();
         int largest = 0;
         int n = arr[0].length;
-
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[row][st.peek()] >= arr[row][i]) {
-                int height = arr[row][st.pop()];
-                int width = st.isEmpty() ? i : i - st.peek() - 1;
-                largest = Math.max(largest, height * width);
+        for(int col = 0; col < n; col++){
+            while(!st.isEmpty() && arr[row][st.peek()] >= arr[row][col]){
+                int index = st.pop();
+                int pse = (st.isEmpty()) ? -1 : st.peek();
+                int width = col-pse-1;
+                int height = arr[row][index];
+                int area = height*width;
+                largest = Math.max(area,largest);
             }
-            st.push(i);
+            st.push(col);
         }
 
-        while (!st.isEmpty()) {
-            int height = arr[row][st.pop()];
-            int width = st.isEmpty() ? n : n - st.peek() - 1;
-            largest = Math.max(largest, height * width);
+        while(!st.isEmpty()){
+            int index = st.pop();
+            int pse = (st.isEmpty()) ? -1 : st.peek();
+            int width = n - pse -1;
+            int height = arr[row][index];
+            int area = height*width;
+            largest = Math.max(area,largest);
         }
 
         return largest;
