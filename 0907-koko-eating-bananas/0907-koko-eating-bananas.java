1a82a7a6e5905ce53396cdceae4638ecdd519f8a
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-24 21:22:53 +0530

    Time: 26 ms (32.79%), Space: 45.5 MB (17.31%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -3,21 +3,19 @@ class Solution {
         if(piles == null || piles.length == 0){
             return 0;
         }
-        int max = 0;
 
+        int low = 1;
+        int high = 0;
         for(int num: piles){
-            max = Math.max(num,max);
+            high = Math.max(num,high);
         }
-        int low  = 1;
-        int high = max;
-        int ans = Integer.MAX_VALUE;
 
-        while(low <=  high){
-            int mid = low + (high - low)/2;
-
-            if(totalTime(piles,mid) <= h){
-                high = mid-1;
+        int ans = 0;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(canEat(piles,mid) <= h){
                 ans = mid;
+                high = mid-1;
             } else {
                 low = mid+1;
             }
@@ -25,12 +23,12 @@ class Solution {
 
         return ans;
     }
-    private int totalTime(int[] piles,int k){
-        int time = 0;
+    private int canEat(int[] piles, int speed){
+        int time  = 0;
         for(int num: piles){
-            time += Math.ceil((double)num/(double)k);
+            time += Math.ceil((double)num/(double)speed);
         }
-        
+
         return time;
     }
 }
