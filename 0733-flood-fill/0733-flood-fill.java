932c682099027e411e73c339ad256f2a81058ffb
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2024-12-25 17:33:11 +0530

    Time: 0 ms (100%), Space: 45 MB (50.5%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,42 +1,25 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
-        int originalColor = image[sr][sc];
-
-        if (m == 0 || originalColor == color) {
+        if(image == null || image.length == 0){
             return image;
         }
 
-        if (originalColor != color) {
-            bfs(image, sr, sc, originalColor, color);
-        }
-
+        int original = image[sr][sc];
+        helper(image,sr,sc,original,color);
         return image;
     }
-
-    private void bfs(int[][] image, int sr, int sc, int originalColor, int color) {
-        int m = image.length;
-        int n = image[0].length;
-
-        int[][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{sr, sc});
-        image[sr][sc] = color;
-
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            for (int[] dir : directions) {
-                int newR = cell[0] + dir[0];
-                int newC = cell[1] + dir[1];
-
-                if (newR < 0 || newR > m - 1 || newC < 0 || newC > n - 1 || image[newR][newC] != originalColor) {
-                    continue;
-                }
-
-                image[newR][newC] = color;
-                q.offer(new int[]{newR, newC});
-            }
+    private void helper(int[][] grid,int row, int col,int original,int color){
+        if(row < 0 || row >= grid.length || col < 0 
+        || col >= grid[0].length || grid[row][col] != original || grid[row][col] == color){
+            return;
         }
+        
+        
+        grid[row][col] = color;
+        helper(grid,row+1,col,original,color);
+        helper(grid,row-1,col,original,color);
+        helper(grid,row,col+1,original,color);
+        helper(grid,row,col-1,original,color);
+        
     }
 }
