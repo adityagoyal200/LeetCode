a0c3f3f0d640cc992cb33dcd2929c7321ef1d3ed
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-01-01 00:02:07 +0530

    Time: 2 ms (72.06%), Space: 42.7 MB (25.2%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,7 +1,7 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         if(grid == null || grid.length == 0){
-            return 0;
+            return -1;
         }
 
         Queue<int[]> q = new LinkedList<>();
@@ -13,37 +13,35 @@ class Solution {
             }
         }
 
-        int cnt = rottOranges(grid,q);
-
+        int cnt = countRotten(grid,q);
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
+                if(grid[i][j] == 1) return -1;
             }
         }
-
-        return cnt == 0 ? 0 : cnt - 1;
+ 
+        return (cnt == 0) ? 0 : cnt-1;
     }
-    private int rottOranges(int[][] grid,Queue<int[]> q){
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        int cnt = 0;
+    public int countRotten(int[][] grid, Queue<int[]> q){
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}}; 
+        int cnt  = 0;
 
         while(!q.isEmpty()){
             int size = q.size();
             for(int i = 0; i < size; i++){
-                int[] cell = q.poll();
+                int[] cell = q.poll(); 
                 for(int[] dir: dirs){
-                    int newR = dir[0] + cell[0];
-                    int newC = dir[1] + cell[1];
-                    if(newR >= 0 && newR < grid.length && newC >= 0 && newC < grid[0].length 
-                    && grid[newR][newC] == 1){
+                    int newR = cell[0] + dir[0];
+                    int newC = cell[1] + dir[1];
+
+                    if(newR >= 0 && newR < m && newC >= 0 && newC < n && grid[newR][newC] == 1){
                         grid[newR][newC] = 2;
                         q.offer(new int[]{newR,newC});
                     }
                 }
             }
-
             cnt++;
         }
 
