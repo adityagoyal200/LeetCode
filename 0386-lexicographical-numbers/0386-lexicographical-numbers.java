b4806dab5b4e166a77b98bb28623e44101acb8f3
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-01-16 00:10:58 +0530

    Time: 5 ms (53.85%), Space: 48.2 MB (83.08%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -4,12 +4,13 @@ class Solution {
         int curr = 1;
         for (int i = 1; i <= n; i++) {
             list.add(curr);
-            if (curr * 10 <= n)
+            if (curr * 10 <= n) {
                 curr = curr * 10;
-            else {
+            } else {
                 while (curr % 10 == 9 || curr >= n) {
                     curr = curr / 10;
                 }
+
                 curr += 1;
             }
         }
