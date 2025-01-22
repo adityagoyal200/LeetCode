1d2b02df8999c9517414140bf32ced5eed1a1417
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-01-22 23:50:43 +0530

    Time: 48 ms (74.86%), Space: 166.4 MB (73.52%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.java
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.java
@@ -1,47 +1,29 @@
 class Solution {
-    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
     public int[][] highestPeak(int[][] isWater) {
-        if(isWater == null || isWater.length == 0){
-            return isWater;
-        }
-
+        int m = isWater.length, n = isWater[0].length;
         Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[isWater.length][isWater[0].length];
-
-        for(int i = 0; i < isWater.length; i++){
-            for(int j = 0; j < isWater[0].length; j++){
-                if(isWater[i][j] == 1){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isWater[i][j] == 1) {
                     isWater[i][j] = 0;
-                    visited[i][j] = true;
-                    q.offer(new int[]{i,j});
+                    q.offer(new int[]{i, j});
                 } else {
                     isWater[i][j] = -1;
                 }
             }
         }
-
-        updateMatrix(isWater,visited,q);
-
-        return isWater;
-    }
-    private void updateMatrix(int[][] grid,boolean[][] visited, Queue<int[]> q){
-        int dist  = 1;
-        while(!q.isEmpty()){
-            int size =  q.size();
-            for(int i = 0; i < size; i++){
-                int[] cell = q.poll();
-                for(int[] dir: directions){
-                    int newR = cell[0] +dir[0];
-                    int newC = cell[1] + dir[1];
-                    if(newR >= 0 && newR < grid.length && newC >= 0 && newC < grid[0].length && !visited[newR][newC]){
-                        grid[newR][newC] = dist;
-                        visited[newR][newC] = true;
-                        q.offer(new int[]{newR,newC});
-                    }
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        while (!q.isEmpty()) {
+            int[] cell = q.poll();
+            for (int[] dir : directions) {
+                int r = cell[0] + dir[0], c = cell[1] + dir[1];
+                if (r >= 0 && r < m && c >= 0 && c < n && isWater[r][c] == -1) {
+                    isWater[r][c] = isWater[cell[0]][cell[1]] + 1;
+                    q.offer(new int[]{r, c});
                 }
             }
-
-            dist++;  
         }
+        
+        return isWater;
     }
 }
