acd2eb36a336220a552fe9004ff1f94e480ff76e
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-01-27 22:18:42 +0530

    Time: 5 ms (87.09%), Space: 45.5 MB (52.06%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,31 +1,27 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> st = new Stack<>();
-        for(int ast: asteroids){
-            if(st.isEmpty() || ast > 0){
+        for (int ast : asteroids) {
+            if (st.isEmpty() || ast > 0) {
                 st.push(ast);
             } else {
-                if(ast > 0){
+                while (!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(ast)) {
+                    st.pop();
+                }
+                if (st.isEmpty() || st.peek() < 0) {
                     st.push(ast);
-                } else {
-                    while(!st.isEmpty() && st.peek() > 0 && st.peek() < Math.abs(ast)){
-                        st.pop();
-                    }
-                    if(st.isEmpty() || st.peek() < 0){
-                        st.push(ast);
-                    } else if(st.peek() == Math.abs(ast)){
-                        st.pop();
-                    }
+                } else if (st.peek() == Math.abs(ast)) {
+                    st.pop();
                 }
             }
+
         }
 
         int[] result = new int[st.size()];
         for (int i = st.size() - 1; i >= 0; i--) {
             result[i] = st.pop();
         }
-        
-        return result;
 
+        return result;
     }
 }
