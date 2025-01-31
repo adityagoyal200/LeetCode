9186d955d2ae2e628d2022ef67e86f01349a7e2b
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-01-31 17:48:02 +0530

    Time: 829 ms (90.12%), Space: 0B (100%) - LeetHub

--- 1541-top-travellers/1541-top-travellers.sql
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -1,11 +1,8 @@
-SELECT u.name, CASE 
-        WHEN SUM(r.distance) IS NULL THEN 0
-        ELSE SUM(r.distance)
-        END AS travelled_distance
+# Write your MySQL query statement below
+SELECT u.name, coalesce(SUM(r.distance), 0) AS travelled_distance
 FROM users u
 LEFT JOIN rides r ON u.id = r.user_id
 GROUP BY u.id
 ORDER BY travelled_distance DESC, u.name ASC;
-
 
 
