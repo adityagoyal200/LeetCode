a7a5e203b479701b55d633bc46e3fb34a64c0db6
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-02-06 08:45:13 +0530

    Time: 0 ms (100%), Space: 12.8 MB (5.65%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        nums.sort()
+        result = []
+        
+        def backtrack(start, path):
+            result.append(path[:])
+            
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                backtrack(i + 1, path + [nums[i]])
+        
+        backtrack(0, [])
+        return result
