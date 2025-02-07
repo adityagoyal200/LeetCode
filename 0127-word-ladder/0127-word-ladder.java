fc47e44b30b38d8de39fe45db87a0502adf62a58
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-02-07 12:00:24 +0530

    Time: 65 ms (83.93%), Space: 46 MB (79.04%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,40 +1,38 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if(beginWord.length() != endWord.length()){
-            return 0;
-        }
+        if(beginWord.length() != endWord.length()) return 0;
 
         Set<String> words = new HashSet<>(wordList);
         if(!words.contains(endWord)) return 0;
 
-        int count = helper(beginWord,endWord,words);
-
-        return (count == 0) ? 0 : count;
-    }
-    private int helper(String begin, String end, Set<String> words){
         int cnt = 1;
         Queue<String> q = new LinkedList<>();
-        q.offer(begin);
+        q.offer(beginWord);
+        words.remove(beginWord);
 
         while(!q.isEmpty()){
             int size = q.size();
+
             for(int i = 0; i < size; i++){
+
                 String currWord = q.poll();
-                if(currWord.equals(end)) return cnt;
+                if(currWord.equals(endWord)) return cnt;
                 char[] wordArray = currWord.toCharArray();
+
                 for(int j = 0; j < wordArray.length; j++){
-                    char curr = wordArray[j];
+                    char orignalChar = wordArray[j];
+                    
                     for(char c = 'a'; c <= 'z'; c++){
-                        if(c == curr) continue;
+                        if(c == orignalChar) continue;
                         wordArray[j] = c;
-                        String newWord = new String(wordArray);
-                        if(words.contains(newWord)){
-                            q.offer(newWord);
-                            words.remove(newWord);
+                        String nextWord = new String(wordArray);
+                        if(words.contains(nextWord)){
+                            q.offer(nextWord);
+                            words.remove(nextWord);
                         }
                     }
 
-                    wordArray[j] = curr;
+                    wordArray[j] = orignalChar;
                 }
             }
 
