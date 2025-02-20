200d9984db201698f99ddb3ed3d52353810e2a9f
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-02-20 14:27:27 +0530

    Time: 0 ms (100%), Space: 41.5 MB (77.55%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.java
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -1,10 +1,7 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         StringBuilder sb = new StringBuilder();
-        
-        // Use diagonal flipping to ensure uniqueness
         for (int i = 0; i < nums.length; i++) {
-            // Flip the ith bit of the ith string
             sb.append(nums[i].charAt(i) == '0' ? '1' : '0');
         }
         
