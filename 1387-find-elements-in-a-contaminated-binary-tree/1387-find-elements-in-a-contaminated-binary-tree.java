6b180e5265bff150c502bb71901cf949586149a2
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-02-21 14:22:44 +0530

    Time: 20 ms (98.92%), Space: 47.9 MB (27.76%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -1,46 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class FindElements {
-    HashSet<Integer> set;
-    TreeNode root;
+    private final HashSet<Integer> set = new HashSet<>();
+
     public FindElements(TreeNode root) {
-        set = new HashSet<>();
-        this.root = root;
-        root.val = 0;
         set.add(0);
-        dfs(root);
+        root.val = 0;
+        recover(root);
     }
-    
+
     public boolean find(int target) {
         return set.contains(target);
     }
 
-    private void dfs(TreeNode root){
-        if(root == null) return;
-        
-        if(root.right != null){
-            root.right.val = (root.val * 2) + 2;
-            set.add(root.right.val);
-            dfs(root.right);
+    private void recover(TreeNode node) {
+        if (node.left != null) {
+            node.left.val = (node.val << 1) + 1;
+            set.add(node.left.val);
+            recover(node.left);
         }
-        if(root.left != null){
-            root.left.val = (root.val*2) + 1;
-            set.add(root.left.val);
-            dfs(root.left);
-        }   
+        if (node.right != null) {
+            node.right.val = (node.val << 1) + 2;
+            set.add(node.right.val);
+            recover(node.right);
+        }
     }
 }
 
