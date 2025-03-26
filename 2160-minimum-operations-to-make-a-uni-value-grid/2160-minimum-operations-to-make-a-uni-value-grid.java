8d2964b1065e60eb501397a944e00637251d1e76
Aditya goyal <59175929+adityagoyal200@users.noreply.github.com>
2025-03-26 14:14:20 +0530

    Time: 127 ms (7.54%), Space: 72.5 MB (70.85%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,66 +1,28 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        int m = grid.length, n = grid[0].length;
-        int total = m * n;
-        int[] values = new int[total];
-        int index = 0;
-        int base = grid[0][0];
-
+        List<Integer> values = new ArrayList<>();
+        
         for (int[] row : grid) {
             for (int val : row) {
-                if ((val - base) % x != 0) {
-                    return -1;
-                }
-                values[index++] = val;
+                values.add(val);
             }
         }
 
-        int median = quickselect(values, total / 2, 0, total - 1);
-        
+        Collections.sort(values);
 
+        for (int val : values) {
+            if (Math.abs(val - values.get(0)) % x != 0) {
+                return -1;
+            }
+        }
+
+        int median = values.get(values.size() / 2);
         int operations = 0;
+
         for (int val : values) {
             operations += Math.abs(val - median) / x;
         }
-        
+
         return operations;
-    }
-    
-    private int quickselect(int[] nums, int k, int left, int right) {
-        if (left == right) return nums[left];
-        
-        Random rand = new Random();
-        int pivotIndex = left + rand.nextInt(right - left);
-        pivotIndex = partition(nums, left, right, pivotIndex);
-        
-        if (k == pivotIndex) {
-            return nums[k];
-        } else if (k < pivotIndex) {
-            return quickselect(nums, k, left, pivotIndex - 1);
-        } else {
-            return quickselect(nums, k, pivotIndex + 1, right);
-        }
-    }
-    
-    private int partition(int[] nums, int left, int right, int pivotIndex) {
-        int pivotValue = nums[pivotIndex];
-        swap(nums, pivotIndex, right);
-        int storeIndex = left;
-        
-        for (int i = left; i < right; i++) {
-            if (nums[i] < pivotValue) {
-                swap(nums, storeIndex, i);
-                storeIndex++;
-            }
-        }
-        
-        swap(nums, right, storeIndex);
-        return storeIndex;
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
